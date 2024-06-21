cabf9bf3b83ca5bdbd5acca28f433e54ebe86d05
805-16 <805-16@DESKTOP-J4O9QAK>
2024-06-21 11:02:45 +0900

    2024-06-21 과제1[Ex01]완료

---
+++ src/com/javaex/practice/Ex01.java
@@ -0,0 +1,19 @@
+package com.javaex.practice;
+
+//클래스
+public class Ex01 {
+	
+	//메소드
+	public static void main(String[] args) {
+		
+		
+	/*다음중 기본 자료형중 정수형자료형이 아닌 것은?
+	   
+	   -->정답: (2) float
+	 
+	  */
+		
+		
+	}
+
+}
