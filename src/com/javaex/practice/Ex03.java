2a52a30a6be7558a9ab2871379bc4613bc885da6
hje-1998 <lg01022670655@gmail.com>
2024-06-21 15:26:37 +0900

    2024-06-21 과제1[Ex03]완료

---
+++ src/com/javaex/practice/Ex03.java
@@ -0,0 +1,15 @@
+package com.javaex.practice;
+
+public class Ex03 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		/* JVM에 대한 설명으로 틀린 것을 찾아 수정해보세요
+		 * 
+		 * -->정답: (3) 운영체제별로 다른 JVM이 사용된다.
+		 * 
+		 */
+	}
+
+}
